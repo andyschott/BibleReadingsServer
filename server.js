aac8fbc4c56814156b0a7710ee8ae1cf2c9e17dd
Andy Schott <andy@andyschott.com>
2014-09-01 09:39:09 -0400

    add simple route to return the readings for a day

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/', function(req, res) {
   });
 });
 
+// Set Andy as the last reader today
 app.post('/andy', function(req, res) {
   var reader = {
     "name" : "Andy",
@@ -30,6 +31,7 @@ app.post('/andy', function(req, res) {
   });
 });
 
+// Set Melissa as the reader today
 app.post('/melissa', function(req, res) {
   var reader = {
     "name" : "Melissa",
@@ -38,6 +40,11 @@ app.post('/melissa', function(req, res) {
   fs.writeFile(dataFile, JSON.stringify(reader), function(err) {
     res.send('OK');
   });
+});
+
+// Get the reading(s) for a given day
+app.get('/reading/:month/:day', function(req, res) {
+  res.send("Returning readings for month " + req.params.month + " and day " + req.params.day + ".");
 });
 
 app.listen(process.argv[2] || 3000);

227ff7dc2a11468854fd7f5f7d1e32ce25627d33
Andy Schott <andy@andyschott.com>
2014-10-04 12:15:26 -0400

    create link to open all readings

--- public/app.js
+++ public/app.js
@@ -11,6 +11,16 @@ window.app = function() {
       return encodeURI(url);
     },
 
+    createAllReadingsLink: function(readings) {
+      var titles = [];
+      for(var i = 0; i < readings.length; i++) {
+        titles.push(this.buildReadingTitle(readings[i]));
+      }
+
+      var url = baseUrl + titles.join(';');
+      return encodeURI(url);
+    },
+
     buildReadingTitle : function(reading) {
       if(reading.start_chapter === reading.end_chapter) {
         if(reading.start_verse === reading.end_verse) {
@@ -41,6 +51,7 @@ window.app = function() {
 
         var readingsList = document.getElementById('readings');
 
+        // Add all readings to the list
         for(var i = 0; i < readings.length; i++) {
           var li = document.createElement('li');
           var a = document.createElement('a');
@@ -49,6 +60,11 @@ window.app = function() {
           li.appendChild(a);
           readingsList.appendChild(li);
         }
+
+        // Build the link to open all readings at once
+        var url = self.createAllReadingsLink(readings);
+        var openAllReadings = document.getElementById('openAllReadings');
+        openAllReadings.href = url;
       };
       req.send();
     }
